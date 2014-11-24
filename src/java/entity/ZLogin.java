6324621f55eaabb8537e646d8a77cb6d17a54724
macuser <macuser@192.168.11.8>
2014-11-24 22:53:57 +0900

    初期登録

---
+++ src/java/entity/ZLogin.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author macuser
+ */
+@Entity
+@Table(name = "Z_LOGIN")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ZLogin.findAll", query = "SELECT z FROM ZLogin z"),
+    @NamedQuery(name = "ZLogin.findById", query = "SELECT z FROM ZLogin z WHERE z.id = :id")})
+public class ZLogin implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Long id;
+
+    public ZLogin() {
+    }
+
+    public ZLogin(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ZLogin)) {
+            return false;
+        }
+        ZLogin other = (ZLogin) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.ZLogin[ id=" + id + " ]";
+    }
+    
+}
